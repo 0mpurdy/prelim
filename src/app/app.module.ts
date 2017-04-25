73f299011a87de3562845d14574593742bb0b1c4
0mpurdy <0mpurdy@gmail.com>
2017-04-25 20:38:42 +0100

    Add Sign Up page
    Add angular material and animations for use in the project.
    Move welcome screen into separate component.
    Add routing.
    Add sign up page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdCardModule, MdInputModule, MdButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+export const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MdCardModule,
+    MdInputModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
